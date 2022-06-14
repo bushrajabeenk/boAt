a3e8688fb841c6babf269431c7c3b0ae57aadf4c
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-06-14 12:35:13 +0530

    changes

---
+++ boat/src/Components/Homepage/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "./../Footer/Footer";
+
+const Homepage = () => {
+  return (
+    <div>
+      
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Homepage;
