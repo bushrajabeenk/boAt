a8f30a0ad1d299253f8db879834ac2c329def50b
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-06-17 19:57:54 +0530

    ch

--- boat/src/Store/cart/cart.reducer.js
+++ boat/src/Store/cart/cart.reducer.js
@@ -18,18 +18,22 @@ import {
 const initState = {
   getData: {
     loading: false,
+    success: false,
     error: false,
   },
   postData: {
     loading: false,
+    success: false,
     error: false,
   },
   patchData: {
     loading: false,
+    success: false,
     error: false,
   },
   deleteData: {
     loading: false,
+    success: false,
     error: false,
   },
   data: [],
@@ -39,28 +43,144 @@ const initState = {
 export const cartReducer = (state = initState, { type, payload }) => {
   switch (type) {
     case GET_DATA_LOADING: {
+      return {
+        ...state,
+        getData: {
+          ...state.getData,
+          loading: true,
+          success: false,
+          error: false,
+        },
+      };
     }
     case GET_DATA_SUCCESS: {
+      return {
+        ...state,
+        getData: {
+          ...state.getData,
+          loading: false,
+          success: true,
+          error: false,
+        },
+        data: payload,
+      };
     }
     case GET_DATA_ERROR: {
+      return {
+        ...state,
+        getData: {
+          ...state.getData,
+          loading: false,
+          success: false,
+          error: true,
+        },
+      };
     }
     case POST_DATA_LOADING: {
+      return {
+        ...state,
+        postData: {
+          ...state.postData,
+          loading: true,
+          success: false,
+          error: false,
+        },
+      };
     }
     case POST_DATA_SUCCESS: {
+      return {
+        ...state,
+        postData: {
+          ...state.postData,
+          loading: false,
+          success: true,
+          error: false,
+        },
+        data: [...state.data, payload],
+      };
     }
     case POST_DATA_ERROR: {
+      return {
+        ...state,
+        postData: {
+          ...state.postData,
+          loading: false,
+          success: false,
+          error: true,
+        },
+      };
     }
     case PATCH_DATA_LOADING: {
+      return {
+        ...state,
+        patchData: {
+          ...state.patchData,
+          loading: true,
+          success: false,
+          error: false,
+        },
+        data: payload,
+      };
     }
     case PATCH_DATA_SUCCESS: {
+      return {
+        ...state,
+        patchData: {
+          ...state.patchData,
+          loading: false,
+          success: true,
+          error: false,
+        },
+        data: payload,
+      };
     }
     case PATCH_DATA_ERROR: {
+      return {
+        ...state,
+        patchData: {
+          ...state.patchData,
+          loading: false,
+          success: false,
+          error: true,
+        },
+        data: payload,
+      };
     }
     case DELETE_DATA_LOADING: {
+      return {
+        ...state,
+        deleteData: {
+          ...state.deleteData,
+          loading: true,
+          success: false,
+          error: false,
+        },
+        data: payload,
+      };
     }
     case DELETE_DATA_SUCCESS: {
+      return {
+        ...state,
+        deleteData: {
+          ...state.deleteData,
+          loading: false,
+          success: true,
+          error: false,
+        },
+        data: payload,
+      };
     }
     case DELETE_DATA_ERROR: {
+      return {
+        ...state,
+        deleteData: {
+          ...state.deleteData,
+          loading: false,
+          success: false,
+          error: true,
+        },
+        data: payload,
+      };
     }
     case INC_DATA: {
       return { ...state, count: state.count + payload };
